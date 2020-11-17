15f106e88c3fb34b88c72f126e4cb7a25784fa44
Daniel Diaz Platon <daniel_dp96@hotmail.com>
2020-11-17 17:22:17 -0600

    Chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -4,7 +4,7 @@ var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre es necesario y sala son necesarios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
@@ -12,13 +12,16 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-
-        console.log('Usuarios Conectados', resp);
+        // console.log('Usuarios conectados', resp);
+        rederizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -39,21 +42,21 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Escuchar Cambios de usuario Entra o sale
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
-
+    // console.log(personas);
+    rederizarUsuarios(personas);
 });
 
-// Mensaje privado
-
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
 
-    console.log('Mensaje privado: ', mensaje);
+    console.log('Mensaje Privado:', mensaje);
+
 });
